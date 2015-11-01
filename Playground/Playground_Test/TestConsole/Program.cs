94add77e39621770d11ba9bf993607fa0e35f569
Jordan Knight <jakkaj@gmail.com>
2015-11-01 11:52:44 +1100

    Demo to tick throught the day

--- Playground/Playground_Test/TestConsole/Program.cs
+++ Playground/Playground_Test/TestConsole/Program.cs
@@ -13,21 +13,26 @@ namespace TestConsole
 {
     class Program
     {
+        private static Strip _strip;
         static void Main(string[] args)
         {
-            _stripTest();
-            //_writer();
+            var p = new StripWriter("COM4");
+
+            _strip = new Strip(StripSize.Size_144, p);
+
+            //_stripTest();
+            _writer();
             while (true)
             {
                 Thread.Sleep(1000);
             }
+
+
         }
 
         static async void _stripTest()
         {
-            var p = new StripWriter("COM4");
 
-            var strip = new Strip(StripSize.Size_144, p);
 
             //strip.Rainbow();
 
@@ -35,45 +40,27 @@ namespace TestConsole
 
             while (true)
             {
-                
 
-                strip.Clear();
+
+                _strip.Clear();
                 //await Task.Delay(500);
-                
-                strip.SetDot(rnd.Next(0, 120), 255, 0, 0);
-                strip.SetDot(rnd.Next(0, 120), 0, 0, 255);
-                strip.FlashDot(rnd.Next(0, 120), 0, 255, 0);
-                strip.SetRange(rnd.Next(0, 120), 10, 255, 165, 0);
-                strip.SetRange(rnd.Next(0, 120), 10, 255, 165, 0);
-                strip.FlashRange(rnd.Next(0, 120), 12, 255, 0, 0);
+
+                _strip.SetDot(rnd.Next(0, 120), 255, 0, 0);
+                _strip.SetDot(rnd.Next(0, 120), 0, 0, 255);
+                _strip.FlashDot(rnd.Next(0, 120), 0, 255, 0);
+                _strip.SetRange(rnd.Next(0, 120), 10, 255, 165, 0);
+                _strip.SetRange(rnd.Next(0, 120), 10, 255, 165, 0);
+                _strip.FlashRange(rnd.Next(0, 120), 12, 255, 0, 0);
 
                 await Task.Delay(6000);
             }
 
-            
+
         }
 
         async static void _writer()
         {
-            var port = new SerialPort();
-
-            foreach (var p in SerialPort.GetPortNames())
-            {
-                Console.WriteLine(p);
-            }
-
-            port.BaudRate = 9600;
-            port.PortName = "COM4";
-
-            port.Open();
-            var open = port.IsOpen;
-
-            //for (var i = 0; i < 144; i++)
-            //{
-            //    port.Write($">1,{i},0,0,255<");
-            //    await Task.Delay(40);
-            //}
-
+            _strip.Clear();
             while (true)
             {
                 var tNow = DateTime.Now;
@@ -84,16 +71,21 @@ namespace TestConsole
 
                 var minutes = tSince.TotalMinutes;
 
-                var actual = (minutes / 760) * 144;
-                Debug.WriteLine(actual);
-                var actualAbs = (int)actual;
+                var actual = (minutes / 720) * 144;
 
-                port.Write($">1,{actualAbs},0,255,0<");
+                Debug.WriteLine(actual);
+
+                var actualAbs = (int)actual;
+               
+                _strip.FlashDot(actualAbs, 0, 255, 0);
+
+                if (actualAbs > 0)
+                {
+                    _strip.SetDot(actualAbs - 1, 0, 0, 10);
+                }
                 await Task.Delay(5000);
             }
-
             
-            port.Close();
         }
     }
 }
