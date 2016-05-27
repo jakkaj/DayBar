561d39de7be1867bef938948a0f8c5c77e6b5644
Jordan Knight <jakkaj@gmail.com>
2016-05-27 14:25:23 +1000

    Added POC of daybar on wpf

---
+++ DaybarWPF/DaybarWPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DaybarWPF.Util;
+
+namespace DaybarWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Activated += MainWindow_Activated;
+            
+        }
+
+        async void _top()
+        {
+            while (true)
+            {
+                await Task.Delay(500);
+                this.Topmost = true;
+            }
+        }
+
+       
+
+        private void MainWindow_Activated(object sender, EventArgs e)
+        {
+            _position();
+        }
+
+        void _position()
+        {
+            var tb = WindowsTaskbar.TaskbarPostion;
+            this.Left = 0;
+            this.Top = 0;
+            this.Width = 0;
+            this.Height = 0;
+
+            var pLeft = this.PointFromScreen(new Point(tb.Left, tb.Top));
+            var pHeight = this.PointFromScreen(new Point(tb.Width, tb.Height));
+            
+            this.Left = pLeft.X;
+            this.Top = pLeft.Y - 5;
+            this.Height = 2;
+            this.Width = pHeight.X;
+            var t = tb;
+        }
+    }
+}
---
+++ DaybarWPF/DaybarWPF/Util/TaskBarUtils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DaybarWPF.Util
+{
+
+
+    //this is from https://winsharp93.wordpress.com/2009/06/29/find-out-size-and-position-of-the-taskbar/
+    
+        public sealed class WindowsTaskbar
+        {
+            private const string ClassName = "Shell_TrayWnd";
+
+            public static Rectangle TaskbarPostion
+            {
+                get
+                {
+                    IntPtr taskbarHandle = User32.FindWindow(WindowsTaskbar.ClassName, null);
+
+                    RECT r = new RECT();
+                    User32.GetWindowRect(taskbarHandle, ref r);
+
+                    return Rectangle.FromLTRB(r.left, r.top, r.right, r.bottom);
+                }
+            }
+        }
+
+        public static class User32
+        {
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+    }
+
